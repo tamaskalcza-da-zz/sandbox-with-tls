39be3782c7c6f64ea18d594041c892db3fe6f70f
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2020-11-17 10:51:36 +0100

    Specifying CN in certificate subject.

--- generate-tls-files.sh
+++ generate-tls-files.sh
@@ -18,25 +18,28 @@ function generate_key_and_signed_certificate() {
   local certificate=$2
   local ca_key=$3
   local ca_certificate=$4
-  openssl req -newkey rsa:$bit_length -nodes -keyout "$key" -out "$request" -subj "/O=Generator Script"
-  openssl x509 -req -days 365 -in "$request" -CA "$ca_certificate" -CAkey "$ca_key" -set_serial 01 -issuer -out "$certificate"
+  local cn=$5
+  openssl req -newkey rsa:$bit_length -nodes -keyout "$key" -out "$request" -subj "/CN=$cn"
+  openssl x509 -req -days 365 -in "$request" -CA "$ca_certificate" -CAkey "$ca_key" -set_serial 01 -issuer -out "$certificate" \
+    -extfile "openssl-extensions.cnf" \
+    -extensions req_ext
   rm $request
 }
 
 log "Generating CA key and self-signed certificate..."
 ca_key="$work_dir/ca.key"
 ca_certificate="$work_dir/ca.crt"
-openssl req -newkey rsa:$bit_length -nodes -keyout "$ca_key" -x509 -days 365 -out "$ca_certificate" -subj "/O=Generator Script"
+openssl req -newkey rsa:$bit_length -nodes -keyout "$ca_key" -x509 -days 365 -out "$ca_certificate" -subj "/CN=0.0.0.0.ca"
 
 log "Generating server key and certificate signed by CA..."
 server_key="$work_dir/server.key"
 server_certificate="$work_dir/server.crt"
-generate_key_and_signed_certificate $server_key $server_certificate $ca_key $ca_certificate
+generate_key_and_signed_certificate $server_key $server_certificate $ca_key $ca_certificate "0.0.0.0.server"
 
 log "Generating client key and certificate signed by CA..."
 client_key="$work_dir/client.key"
 client_certificate="$work_dir/client.crt"
-generate_key_and_signed_certificate $client_key $client_certificate $ca_key $ca_certificate
+generate_key_and_signed_certificate $client_key $client_certificate $ca_key $ca_certificate "0.0.0.0.client"
 
 log "Generated values:"
 printf -- "- Trust cert collection file: %s\n" $ca_certificate
